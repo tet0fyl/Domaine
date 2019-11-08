97fb6da4a3f1f17ff74d8c7bf8fed4420febe075
tet0 <theophile.braye@gmail.com>
2019-11-08 10:41:42 +0100

    Chg de nom asset->ressources

--- views/objects/evenement_object.php
+++ views/objects/evenement_object.php
@@ -1,6 +1,6 @@
 <article>
 	<img alt="<?php echo $objEvenement->getTitle(); ?>" 
-		 width="500" src="./asset/images/event/<?php echo $objEvenement->getImg(); ?>">
+		 width="500" src="./ressources/images/event/<?php echo $objEvenement->getImg(); ?>">
 	<div>
 		<h3><?php echo $objEvenement->getTitle(); ?></h3>
 		<p><?php echo $objEvenement->getDate() ?></p>
