8adcd8e1372691638ca961f6fb2eec24482c6846
Sephora <karkarsephora@gmail.com>
2019-11-29 00:21:17 +0100

    maj footer

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,30 @@
 		<footer>
-            <div>
-                <p>Le Domaine</p>
-                <p>Rue</p>
-                <p>Ville</p>
+            <div id="conteneur">
+
+            <div class="logo-footer">
+                <img src="ressources/images/logo/logo.png" width="120" height="120">
+            </div>
+
+            <div class="newsletter">
+                <label>Newsletter : </label><br>
+                <input type="text" name="email" placeholder="E-mail">
+            </div>
+
+            <div class="coordonnees">
+                <p>Le Domaine <br>
+                Rue<br>
+                Ville<br>
+                </p>
+            </div>
+
+           <div class="informations">
+               <ul>
+                   <li><a>Mentions légales</a></li>
+                   <li><a>Conditions générales</a></li>
+                   <li><a>Plan du site</a></li>
+               </ul>
+           </div>
+
             </div>
 		</footer>
 	</body>
