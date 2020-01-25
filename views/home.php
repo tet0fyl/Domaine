bb0279bb337655daba2d0d758c07cc109a0a4e22
tet0 <theophile.braye@gmail.com>
2020-01-25 23:45:17 +0100

    petit ajustement du slider home media

--- views/home.php
+++ views/home.php
@@ -1,5 +1,5 @@
 <!-- ///////// SLIDER MEDIA ///////// -->
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<div id="myCarousel" class="carousel slide bg-dark" data-ride="carousel">
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
