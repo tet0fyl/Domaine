3bc8d51849295c14bf6c74d6a40e2c18dfecf5fc
tet0 <theophile.braye@gmail.com>
2020-01-25 02:32:40 +0100

    CSS Nav Bar Debut

--- views/admin/admin_portail.php
+++ views/admin/admin_portail.php
@@ -1,15 +1,17 @@
-<h1>Espace Admin</h1>
-<div>
+<div class="container">
+    <h1>Portail Admin</h1>
 <form method="post" action="admin-connexion">
-    <div>
+    <div class="form-group">
     <label for="identifiant">Identifiant</label>
-    <input type="text" name="identifiant">
+    <input class='form-control' type="text" name="identifiant">
     </div>
-    <div>
+    <div class="form-group">
     <label for="password" >Mot de passe : </label>
-    <input type="password" name="password">
+    <input class='form-control' type="password" name="password">
     </div>
-    <input type="submit">
+    <div class="form-group">
+        <input class='btn btn-primary' type="submit">
+    </div>
 </form>
     <?php if(isset($data['error']) && $data['error']){?>
     <p style="color: red">Login ou Mot de passe incorrect</p>
