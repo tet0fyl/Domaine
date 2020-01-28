cd88deb1ccae09902617d99a8344782223e52fb6
Sephora <karkarsephora@gmail.com>
2020-01-28 10:43:42 +0100

    ajustement du footer et du body

--- views/header.php
+++ views/header.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="ressources/styles/styles.css">
 		<link rel="icon" type="image/png" href="ressources/images/logo/logo.png" />
     </head>
-	<body>
+	<body class="min-vh-100 d-flex flex-column justify-content-between">
 		<header>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
