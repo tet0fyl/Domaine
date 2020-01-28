0f6ed613bb94f7f54a7bdc8e446c607e7fdd8fd6
Indigoblin <axolot.jvc@gmail.com>
2020-01-28 12:08:18 +0100

    change shadow hover

--- views/objects/own_page/presse_object.php
+++ views/objects/own_page/presse_object.php
@@ -1,9 +1,9 @@
 <article>
-        <div class="shadow p-2 mb-3 bg-white rounded d-flex flex-column">
+        <div class="hoovshad shadow p-2 mb-3 bg-white rounded d-flex flex-column">
             <?php
             if(isset($_SESSION['admin']) && $_SESSION['admin']) {
                 adminOption('presse',$objPresse);
             } ?>
-            <a href="<?php echo $urlFichierPresse . $objPresse->getFichier() ?>" target="_blank"><?php echo $objPresse->getFichier(); ?></a>
+            <a class="" href="<?php echo $urlFichierPresse . $objPresse->getFichier() ?>" target="_blank"><?php echo $objPresse->getFichier(); ?></a>
         </div>
 </article>
